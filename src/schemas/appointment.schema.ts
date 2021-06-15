dbc123af1e9cf2925181641c46a12c3cf0eda850
Koon Hian <angkoonhian@gmail.com>
2021-06-15 10:17:31 +0800

    first heroku

--- src/schemas/appointment.schema.ts
+++ src/schemas/appointment.schema.ts
@@ -1,3 +1,4 @@
+import { boolean } from 'joi';
 import * as mongoose from 'mongoose';
 
 export interface Iappointment extends mongoose.Document {
@@ -11,3 +12,33 @@ export interface Iappointment extends mongoose.Document {
 }
 
 const Schema = mongoose.Schema
+
+export const AppointmentSchema = new mongoose.Schema({
+    patientId: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    clinicId: {
+        type: Schema.Types.ObjectId,
+        ref: 'clinics'
+    },
+    dateTime: {
+        type: Date,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    readStatus: {
+        type: Boolean,
+        required: true,
+    },
+    status: {
+        tyoe: String,
+        required: true,
+    }
+})
+
+const Appointment = mongoose.model<Iappointment>('appointments', AppointmentSchema);
+export default Appointment
